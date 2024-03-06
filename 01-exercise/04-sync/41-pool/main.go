a59230ca39a21fc49b015eced2cf5651614bf0e9
“Kreagentle” <ponwrt@gmail.com>
2024-03-06 12:35:40 +0000

    Sync pool

--- 01-exercise/04-sync/41-pool/main.go
+++ 01-exercise/04-sync/41-pool/main.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
+	"sync"
 	"time"
 )
 
-//TODO: create pool of bytes.Buffers which can be reused.
+// create pool of bytes.Buffers which can be reused.
+
+var pool = sync.Pool{
+	New: func() interface{} {
+		fmt.Println("allocating a new buffer")
+		return new(bytes.Buffer)
+	},
+}
 
 func log(w io.Writer, val string) {
-	var b bytes.Buffer
+	b := pool.Get().(*bytes.Buffer)
 
+	b.Reset()
 	b.WriteString(time.Now().Format("15:04:05"))
 	b.WriteString(" : ")
 	b.WriteString(val)
 	b.WriteString("\n")
 
 	w.Write(b.Bytes())
+
+	pool.Put(b)
 }
 
 func main() {
